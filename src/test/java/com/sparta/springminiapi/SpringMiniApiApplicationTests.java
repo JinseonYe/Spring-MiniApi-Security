87610345b2289382bad6c75de981e18163543f00
JinseonYe <aatwe24@gmail.com>
2022-12-27 20:59:16 +0900

    Spring-MiniApi 생성

---
+++ src/test/java/com/sparta/springminiapi/SpringMiniApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springminiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMiniApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
